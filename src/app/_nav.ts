9b8ab299ec57cd9b0c2ca0cfd860be55764c86ef
naeem <naeembashir632gmail.com>
2019-10-19 10:10:07 -0700

    naeem

---
+++ src/app/_nav.ts
@@ -0,0 +1,159 @@
+interface NavAttributes {
+  [propName: string]: any;
+}
+interface NavWrapper {
+  attributes: NavAttributes;
+  element: string;
+}
+interface NavBadge {
+  text: string;
+  variant: string;
+}
+interface NavLabel {
+  class?: string;
+  variant: string;
+}
+
+export interface NavData {
+  name?: string;
+  url?: string;
+  icon?: string;
+  badge?: NavBadge;
+  title?: boolean;
+  children?: NavData[];
+  variant?: string;
+  attributes?: NavAttributes;
+  divider?: boolean;
+  class?: string;
+  label?: NavLabel;
+  wrapper?: NavWrapper;
+}
+
+export const navItems: NavData[] = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  {
+    title: true,
+    name: 'Theme'
+  },
+  {
+    name: 'Colors',
+    url: '/theme/colors',
+    icon: 'icon-drop'
+  },
+  {
+    name: 'Typography',
+    url: '/theme/typography',
+    icon: 'icon-pencil'
+  },
+  {
+    title: true,
+    name: 'Components'
+  },
+  {
+    name: 'Base',
+    url: '/base',
+    icon: 'icon-puzzle',
+    children: [
+      {
+        name: 'Cards',
+        url: '/base/cards',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Carousels',
+        url: '/base/carousels',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Collapses',
+        url: '/base/collapses',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Forms',
+        url: '/base/forms',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Pagination',
+        url: '/base/paginations',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Popovers',
+        url: '/base/popovers',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Progress',
+        url: '/base/progress',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Switches',
+        url: '/base/switches',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Tables',
+        url: '/base/tables',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Tabs',
+        url: '/base/tabs',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Tooltips',
+        url: '/base/tooltips',
+        icon: 'icon-puzzle'
+      }
+    ]
+  },
+  
+  
+  
+  
+ 
+  {
+    divider: true
+  },
+  {
+    title: true,
+    name: 'Extras',
+  },
+  {
+    name: 'Pages',
+    url: '/pages',
+    icon: 'icon-star',
+    children: [
+      {
+        name: 'Login',
+        url: '/login',
+        icon: 'icon-star'
+      },
+     
+      {
+        name: 'Error 404',
+        url: '/404',
+        icon: 'icon-star'
+      },
+      {
+        name: 'Error 500',
+        url: '/500',
+        icon: 'icon-star'
+      }
+    ]
+  },
+
+ 
+];
